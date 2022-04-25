f2356b3cf1da5721d58f69ef7b3044ec9f8da245
Marco Lanconelli <m.lanconelli@nier.it>
2022-04-25 12:25:32 +0200

    fallimento nel provare ad ottimizzare (wip)

--- webcrawler.py
+++ webcrawler.py
@@ -7,6 +7,13 @@ from bs4 import BeautifulSoup
 # telegram
 import telebot
 
+# threading / multiprocessing
+import time
+import threading
+from multiprocessing import Process
+import asyncio
+import concurrent.futures
+
 TOKEN = os.getenv('API_TELEGRAM_TOKEN')
 bot = telebot.TeleBot(TOKEN)
 
@@ -28,12 +35,20 @@ class Crawler:
     def get_linked_urls(self, url, html):
         # parsing dell'html per ottenere nuovi riferimenti 
         soup = BeautifulSoup(html, 'html.parser')
-        # posso parallellizzare la ricerca? - ricerca di nuovi link e check della pagina se è presente cosa? mmm
-        for link in soup.find_all('a'):
-            path = link.get('href')
-            if path and path.startswith('/'):
-                path = urljoin(url, path)
-            yield path
+
+        with concurrent.futures.ProcessPoolExecutor(max_workers=20) as executor:
+            future_to_link = {executor.submit(soup.find_all('a', {'class':'s-access-detail-page'})): link for link in soup.find_all('a')}
+            for future in concurrent.futures.as_completed(future_to_link):
+                link  = future_to_link[future]
+                try:
+                    path = link.get('href')
+                    if path and path.startswith('/'):
+                        path = urljoin(url, path)
+                    # self.add_url_to_visit(url)
+                    self.add_url_to_visit(url)
+                except Exception as exc:
+                    logging.error("Expection %s", exc)
+
 
 
     def add_url_to_visit(self, url):
@@ -43,16 +58,18 @@ class Crawler:
     def crawl(self, url):
         html = self.download_url(url)
         # ciclo per aggiungere url parsando gli altri url
-        for url in self.get_linked_urls(url, html):
-            self.add_url_to_visit(url)
+
+        # for url in self.get_linked_urls(url, html):
+        self.get_linked_urls(url, html)
+        
 
 
     def run(self):
+    # def run(self):
         while self.urls_to_visit:
+            # url da visitare
             url = self.urls_to_visit.pop(0)
             logging.info(f'Crawling: {url}')
-            #invio
-            bot.send_message(self.id_msg, f'Crawling: {url}')
 
             try:
                 self.crawl(url)
@@ -60,20 +77,51 @@ class Crawler:
                 logging.exception(f'Failed to crawl: {url}')
             finally:
                 self.visited_urls.append(url)
+                # await queue.put(url)
+        
+        logging.warning(f'Length list: {len(self.urls_to_visit)}')
 
+    def analysis(self):
+        while True:
+            while self.visited_urls:
+                # url = await queue.get()
+                url = self.visited_urls.pop(0)
+                logging.debug(f'Get url by queue {url}')
 
-
-@bot.message_handler(commands=['Greet'])
-def greet(message): 
-    bot.reply_to(message, "Hey, i'm rollo crawler")
+            
 
 @bot.message_handler(commands=['hello'])
 def hello(message): 
-    bot.send_message(message.chat.id, "Hi!:)")
+    bot.reply_to(message, "hey! i'm rollomista's crawler... try this")
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    Crawler(message.chat.id, urls=['https://www.amazon.com/']).run()
+    # asyncio.run(main(message.chat.id))
+    main(message.chat.id)
+
+def main(id_msg):
+    start = time.time()
+    # queue = asyncio.Queue()
+    crawler = Crawler(id_msg, urls=['https://www.unieuro.it/online/'])
+    
+
+    pidList = []
+
+    #compute(currRange)
+    pid1 = Process(target=crawler.run, args=())
+    pid1.start()
+    pidList.append(pid1)        
+    pid2 = Process(target=crawler.analysis, args=())
+    pid2.start()
+    pidList.append(pid2)        
+        
+    for pid in pidList:
+        pid.join()
+
+    end = time.time()
+    logging.info("Elapsed {0} seconds".format(end-start))
+
+    bot.send_message(id_msg, "Il crawling è terminato: {0}".format(end-start))
 
 
 if __name__ == '__main__':
